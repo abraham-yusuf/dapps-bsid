e1c4bc697a111c99dcf3a9b4e2b174ff771a9015
Abraham Yusuf <76843145+abraham-yusuf@users.noreply.github.com>
2021-09-15 10:41:30 +0000

    update src file using typescript

---
+++ src/menu.ts
@@ -0,0 +1,72 @@
+// export const menu = [
+//   {
+//     path: "/authentication",
+//     label: "Authentication",
+//     component: Authentication,
+//   },
+//   {
+//     path: "/login",
+//     label: "Login",
+//     component: Login,
+//   },
+//   {
+//     path: "/user",
+//     label: "User",
+//     component: User,
+//   },
+//   {
+//     path: "/query-simple",
+//     label: "Query (simple)",
+//     component: QuerySimple,
+//   },
+//   {
+//     path: "/query-manual",
+//     label: "Query (manual",
+//     component: QueryManual,
+//   },
+//   {
+//     path: "/query-object",
+//     label: "Query (object)",
+//     component: QueryObject,
+//   },
+//   {
+//     path: "/query-live",
+//     label: "Query (live)",
+//     component: QueryLive,
+//   },
+//   {
+//     path: "/subscription",
+//     label: "Subscription",
+//     component: Subscription,
+//   },
+//   {
+//     path: "/file",
+//     label: "File",
+//     component: File,
+//   },
+//   {
+//     path: "/file-ipfs",
+//     label: "File (IPFS)",
+//     component: FileIpfs,
+//   },
+//   {
+//     path: "/cloud",
+//     label: "CloudFunction",
+//     component: CloudFunction,
+//   },
+//   {
+//     path: "/web3",
+//     label: "Web3",
+//     component: Web3,
+//   },
+//   {
+//     path: "/web3Api",
+//     label: "Web3Api",
+//     component: Web3Api,
+//   },
+//   {
+//     path: "/assets",
+//     label: "Assets",
+//     component: Assets,
+//   },
+// ];
